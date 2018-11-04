710ab97c7a4e79bd89bcce462664373fdbfd23ab
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-04 16:49:55 -0500

    Create initial ResourceManager, Process, Resource, and GraphSpace components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,53 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Resource extends React.Component {
+    render() {
+        return <p>Resource {this.props.name}</p>;
+    }
+}
+
+class Process extends React.Component {
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return <p>Process {this.props.name}</p>;
+    }
+}
+
+class GraphSpace extends React.Component {
+    render() {
+        return <canvas id="graph-space" />;
+    }
+}
+
+class ResourceManager extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            processes: [],
+            resources: [],
+            resource_event: [],
+        };
+    }
+
+    render() {
+        return (
+                <div>
+                  <h1>Resource Manager</h1>
+                  <div>
+                  </div>
+                </div>
+        );
+    }
+}
+
+ReactDOM.render(<ResourceManager />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

7fc6ee1786acfe1cb35e4da7c706527276203e99
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-04 17:23:03 -0500

    Create ProcessList and ResourceList components

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ class Resource extends React.Component {
     }
 }
 
+function ResourceList(props) {
+    const resources = props.resources;
+    const resourceListItems = resources.map((resource) =>
+                                           <li key={resource.props.id}>{resource.props.name}</li>
+                                          );
+    return (
+            <ul>{resourceListItems}</ul>
+    );
+}
+
 class Process extends React.Component {
 
     componentDidMount() {
@@ -23,6 +33,16 @@ class Process extends React.Component {
     }
 }
 
+function ProcessList(props) {
+    const processes = props.processes;
+    const processListItems = processes.map((process) =>
+                                           <li key={process.props.id}>{process.props.name}</li>
+                                          );
+    return (
+            <ul>{processListItems}</ul>
+    );
+}
+
 class GraphSpace extends React.Component {
     render() {
         return <canvas id="graph-space" />;
@@ -33,8 +53,16 @@ class ResourceManager extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            processes: [],
-            resources: [],
+            processes: [
+                    <Process id="1" name="init" />,
+                    <Process id="2" name="networking" />,
+                    <Process id="3" name="filesystem" />,
+            ],
+            resources: [
+                    <Resource id="1" name="Disk" />,
+                    <Resource id="2" name="RAM" />,
+                    <Resource id="3" name="NIC" />,
+            ],
             resource_event: [],
         };
     }
@@ -44,6 +72,12 @@ class ResourceManager extends React.Component {
                 <div>
                   <h1>Resource Manager</h1>
                   <div>
+                    <h2>Processes</h2>
+                    <ProcessList processes={this.state.processes} />
+                  </div>
+                  <div>
+                    <h2>Resources</h2>
+                    <ResourceList resources={this.state.resources} />
                   </div>
                 </div>
         );

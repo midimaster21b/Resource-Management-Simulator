1adf7602c43f4f2766d615006d5aa4aede88534b
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-06 01:07:38 -0500

    Dynamically draw processes and resources in canvas

--- src/Graphing.js
+++ src/Graphing.js
@@ -7,6 +7,14 @@ export class GraphSpace extends React.Component {
         this.state = {
             count: 1,
         };
+
+        this.process_height  = 30; // px
+        this.process_width   = 50; // px
+        this.process_row     = 100; // px
+
+        this.resource_height = 30; // px
+        this.resource_width  = 50; // px
+        this.resource_row    = 20; // px
     }
 
     render() {
@@ -29,18 +37,43 @@ export class GraphSpace extends React.Component {
         this.clear();
 
         // Draw updated screen
-        ctx.rect(0,0,150,75);
-        ctx.stroke();
+        const numProcesses = this.props.processes.length;
+        const numResources = this.props.resources.length;
 
         ctx.font = "16px Consolas";
         ctx.fillStyle = "black";
-        // ctx.fillStyle = "white";
-        ctx.fillText(this.state.count, 10, 50);
+        ctx.textAlign="center";
 
-        // ctx.fillStyle = color;
-        // ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+        // Draw the processes
+        const processWidth = parseInt(canvas.width / numProcesses);
 
-        // return <canvas id="graph-space" />;
+        for(let x=0; x<numProcesses; x++) {
+            const startCol = x * processWidth + (0.5 * (processWidth - this.process_width));
+            const textCol  = x * processWidth + (0.5 * processWidth);
+            const textRow  = this.process_row + (0.5 * this.process_height);
+
+            // Draw process rectangle
+            ctx.rect(startCol, this.process_row, this.process_width, this.process_height);
+            ctx.stroke();
+
+            ctx.fillText("p"+x, textCol, textRow);
+        }
+
+        // Draw the resources
+        const resourceWidth = parseInt(canvas.width / numResources);
+
+        for(let x=0; x<numResources; x++) {
+            const startCol = x * resourceWidth + (0.5 * (resourceWidth - this.resource_width));
+            const textCol  = x * resourceWidth + (0.5 * resourceWidth);
+            const textRow  = this.resource_row + (0.5 * this.resource_height);
+
+            // Draw resource rectangle
+            ctx.rect(startCol, this.resource_row, this.resource_width, this.resource_height);
+            ctx.stroke();
+
+            ctx.fillText("r"+x, textCol, textRow);
+        }
+
         return <div></div>;
     }
 

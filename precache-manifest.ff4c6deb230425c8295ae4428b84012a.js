d43798c3ad2dd077ae2685ccead4cf7412d8ff2a
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-08 00:20:35 -0500

    Updates

---
+++ precache-manifest.ff4c6deb230425c8295ae4428b84012a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4706ef342d1f10fc48",
+    "url": "/Resource-Management-Simulator/static/js/runtime~main.6c4706ef.js"
+  },
+  {
+    "revision": "3d664d720de3379483e2",
+    "url": "/Resource-Management-Simulator/static/js/main.3d664d72.chunk.js"
+  },
+  {
+    "revision": "182d0b5a08d320a6112d",
+    "url": "/Resource-Management-Simulator/static/js/1.182d0b5a.chunk.js"
+  },
+  {
+    "revision": "3d664d720de3379483e2",
+    "url": "/Resource-Management-Simulator/static/css/main.37b195fb.chunk.css"
+  },
+  {
+    "revision": "073b2e0b8522b896ebcca4cfa22695e6",
+    "url": "/Resource-Management-Simulator/index.html"
+  }
+];
